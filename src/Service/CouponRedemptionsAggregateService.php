f2ae0b1b3164ea0b1c258e84f9d543f7a7a7f63c
Tolben <vitek.pasek@gmail.com>
2025-10-23 12:19:30 +0200

    chore(codegen): add/update generated PHP (Module, Joins, Repository, Mapper, Exceptions) from schema-map

--- src/Service/CouponRedemptionsAggregateService.php
+++ src/Service/CouponRedemptionsAggregateService.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace BlackCat\Database\Packages\CouponRedemptions\Service;
 
-use BlackCat\Core\Database\Database;
+use BlackCat\Core\Database;
 use BlackCat\Database\Packages\CouponRedemptions\Dto\CouponRedemptionDto;
 use BlackCat\Database\Packages\CouponRedemptions\Mapper\CouponRedemptionDtoMapper;
 use BlackCat\Database\Packages\CouponRedemptions\Repository\CouponRedemptionRepository;
